4abae11bcc5f7d537d21ab00376358b21e354706
Sasha Aleshchenko <45843308+TheBigSasha@users.noreply.github.com>
2023-09-10 02:01:33 -0400

    feat: breaking! change PopupMessage to be permanent, and the disappearing one to DisappearingPopupMessage

--- src/lib/components/molecules/popup-message/PopupMessage.tsx
+++ src/lib/components/molecules/popup-message/PopupMessage.tsx
@@ -1,4 +1,4 @@
-import { FC, PropsWithChildren } from 'react'
+import { CSSProperties, FC, PropsWithChildren } from 'react'
 import styles from './popup-message.module.scss'
 
 export type PopupMessageVariant = 'INFO' | 'WARNING' | 'ERROR' | 'SUCCESS'
@@ -7,10 +7,10 @@ export interface PopupMessageProps {
   label: string
   variant?: PopupMessageVariant
   delay?: 0 | 1 | 5 | 10
-  style?: React.CSSProperties
+  style?: CSSProperties
 }
 
-export const PopupMessage: FC<PropsWithChildren<PopupMessageProps>> = ({
+export const DisappearingPopupMessage: FC<PropsWithChildren<PopupMessageProps>> = ({
   label,
   variant = 'PRIMARY',
   children,
@@ -21,6 +21,23 @@ export const PopupMessage: FC<PropsWithChildren<PopupMessageProps>> = ({
     <div
       style={style}
       className={`${styles.popup_message} ${styles[variant.toLowerCase()]} ${styles[`fade_in_delay_${delay}`]}`}
+    >
+      <h3 className={styles.popup_message_label}>{label}</h3>
+      {children}
+    </div>
+  )
+}
+
+export const PopupMessage: FC<PropsWithChildren<Omit<PopupMessageProps, 'delay'>>> = ({
+  label,
+  variant = 'PRIMARY',
+  children,
+  style,
+}) => {
+  return (
+    <div
+      style={style}
+      className={`${styles.popup_message} ${styles[variant.toLowerCase()]} ${styles['fade_in_delay_null']}`}
     >
       <h3 className={styles.popup_message_label}>{label}</h3>
       {children}
